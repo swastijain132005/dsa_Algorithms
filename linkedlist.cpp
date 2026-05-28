1705f498f926511f73b29861686bbd20c50b764e
Swasti jain <jainsanjna336@gmail.com>
2026-05-28 23:44:30 +0530

    Create linkedlist.cpp

---
+++ linkedlist.cpp
@@ -0,0 +1,13 @@
+ListNode
+{int val;ListNode *next;ListNode(int x) : val(x), next(NULL) {}};
+class Solution {public:ListNode* reverseList(ListNode* head)
+{ListNode* prev = NULL;
+ListNode* curr = head;
+while (curr != NULL)
+{ListNode* future = curr->next;
+curr->next = prev;
+prev = curr;
+curr = future;}
+return prev;}
+};
+Recu
