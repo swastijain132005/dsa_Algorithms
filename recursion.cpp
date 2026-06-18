03ad62bd1245411d849688755ba91cc675b4698e
Swasti jain <jainsanjna336@gmail.com>
2026-06-18 17:32:37 +0530

    Add Sieve of Eratosthenes implementation
    Implemented the Sieve of Eratosthenes algorithm to find all prime numbers up to a given number.

--- recursion.cpp
+++ recursion.cpp
@@ -22,4 +22,47 @@ public:
     }
 };
 
-//permutations 1 and 2
+
+//sieve of eratosthenesis \\
+tc (o(nloglongn))
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> sieve(int n) {
+    vector<bool> isPrime(n + 1, true);
+
+    isPrime[0] = false;
+    if (n >= 1)
+        isPrime[1] = false;
+
+    for (int i = 2; i * i <= n; i++) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+
+    vector<int> primes;
+
+    for (int i = 2; i <= n; i++) {
+        if (isPrime[i]) {
+            primes.push_back(i);
+        }
+    }
+
+    return primes;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> primes = sieve(n);
+
+    for (int p : primes) {
+        cout << p << " ";
+    }
+}
